f7b9b496fac5d572e5a518055f5b821962591858
Victor <1149833549@qq.com>
2022-01-22 21:20:46 +0800

    doing

--- main.py
+++ main.py
@@ -1,8 +1,18 @@
 import os
 import sys
+import importlib
+import logging.config
+import imp
+import argparse
+import time
+import shutil
+import socket
+import tarfile
+import fuzzer
+import driller
 
 
-if __name__ == "__main__"
+if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument('binary')
     parser.add_argument('-g', '--grease-with')
@@ -24,4 +34,113 @@ if __name__ == "__main__"
     parser.add_argument('--length-extension', help="Try extending inputs to driller by this many bytes", type=int)
     args = parser.parse_args()
 
-    if os.path.isfile(os.path
+    if os.path.isfile(os.path.join(os.getcwd(), args.logcfg)):
+        logging.config.fileConfig(os.path.join(os.getcwd(), args.logcfg))
+
+    try: os.mkdir("/dev/shm/work/")
+    except OSError: pass
+
+    if args.helper_module:
+        try:
+            helper_module = importlib.import_module(args.helper_module)
+        except (ImportError, TypeError):
+            helper_module = imp.load_source('fuzzing_helper', args.helper_module)
+    else:
+        helper_module = None
+
+    drill_extension = None
+    grease_extension = None
+
+    if args.grease_with:
+        print ("[*] Greasing...")
+        grease_extension = fuzzer.GreaseCallback(
+            args.grease_with,
+            grease_filter=helper_module.grease_filter if helper_module is not None else None,
+            grease_sorter=helper_module.grease_sorter if helper_module is not None else None
+        )
+    if args.driller_workers:
+        print ("[*] Drilling...")
+        drill_extension = driller.LocalCallback(num_workers=args.driller_workers, worker_timeout=args.driller_timeout, length_extension=args.length_extension)
+
+    stuck_callback = (
+        (lambda f: (grease_extension(f), drill_extension(f))) if drill_extension and grease_extension
+        else drill_extension or grease_extension
+    )
+
+    seeds = None
+    if args.seed_dir:
+        seeds = []
+        print ("[*] Seeding...")
+        for dirpath in args.seed_dir:
+            for filename in os.listdir(dirpath):
+                filepath = os.path.join(dirpath, filename)
+                if not os.path.isfile(filepath):
+                    continue
+                with open(filepath, 'rb') as seedfile:
+                    seeds.append(seedfile.read())
+
+    print ("[*] Creating fuzzer...")
+    fuzzer = fuzzer.Fuzzer(
+        args.binary, args.work_dir, afl_count=args.afl_cores, force_interval=args.force_interval,
+        create_dictionary=not args.no_dictionary, stuck_callback=stuck_callback, time_limit=args.timeout,
+        memory=args.memory, seeds=seeds, timeout=args.run_timeout,
+    )
+
+    # start it!
+    print ("[*] Starting fuzzer...")
+    fuzzer.start()
+
+    #if args.ipython:
+    #    print ("[!]")
+    #    print ("[!] Launching ipython shell. Relevant variables:")
+    #    print ("[!]")
+    #    print ("[!] fuzzer")
+    #    print ("[!] driller_extension")
+    #    print ("[!] grease_extension")
+    #    print ("[!]")
+    #    import IPython; IPython.embed()
+
+    try:
+        print ("[*] Waiting for fuzzer completion (timeout: %s, first_crash: %s)." % (args.timeout, args.first_crash))
+
+        crash_seen = False
+        while True:
+            time.sleep(5)
+            if not crash_seen and fuzzer.found_crash():
+                print ("[*] Crash found!")
+                crash_seen = True
+                if args.first_crash:
+                    break
+            if fuzzer.timed_out():
+                print ("[*] Timeout reached.")
+                break
+    except KeyboardInterrupt:
+        print ("[*] Aborting wait. Ctrl-C again for KeyboardInterrupt.")
+    except Exception as e:
+        print ("[*] Unknown exception received (%s). Terminating fuzzer." % e)
+        fuzzer.kill()
+        if drill_extension:
+            drill_extension.kill()
+        raise
+
+    print ("[*] Terminating fuzzer.")
+    fuzzer.kill()
+    if drill_extension:
+        drill_extension.kill()
+
+    if args.tarball:
+        print ("[*] Dumping results...")
+        p = os.path.join("/tmp/", "afl_sync")
+        try:
+            shutil.rmtree(p)
+        except (OSError, IOError):
+            pass
+        shutil.copytree(fuzzer.out_dir, p)
+
+        tar_name = args.tarball.replace("{}", socket.gethostname())
+
+        tar = tarfile.open("/tmp/afl_sync.tar.gz", "w:gz")
+        tar.add(p, arcname=socket.gethostname()+'-'+os.path.basename(args.binary))
+        tar.close()
+        print ("[*] Copying out result tarball to %s" % tar_name)
+        shutil.move("/tmp/afl_sync.tar.gz", tar_name)
